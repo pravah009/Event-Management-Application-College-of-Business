e081d65b26e0738d7e0a89df862adc67e27db852
pravah009 <pravah.p.m@ou.edu>
2022-10-27 22:18:07 -0500

    update: class added

---
+++ MIS-Prog-App/MIS-Prog-App/Event.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace MIS_Prog_App
+{
+    public class Event
+    {
+        public string Title { get; set; }
+        public string eventType { get; set; }
+        public string eventDept { get; set; }
+        public string Location {get; set;}
+        public string Company{get; set;}
+        public DateTime eventDate { get; set; }
+        public string recruitorName {get; set;}
+        
+        public Event(string title, string type, string loc, string date)
+        {
+            this.Title=title;
+            this.eventType = type;
+            this.eventDept=string.Empty;
+            this.Location = loc;
+            this.eventDate = DateTime.Parse(date);
+            this.Company = string.Empty;
+            this.recruitorName = string.Empty;
+        }
+
+        public override string ToString()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            return $"{Title}" +
+                $"\n{eventType} by {eventDept}" +
+                $"\n{Location} at {eventDate}";
+            Console.ResetColor();
+        }
+    }
+}
