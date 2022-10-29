c45817fea58f338cb992999637fe05720358a75c
Pablo-Gutierrez24 <pablogutierrezfelix@gmail.com>
2022-10-29 00:03:18 -0500

    Push for chukwuma

--- MIS-Prog-App/MIS-Prog-App/mainwindow.xaml.cs
+++ MIS-Prog-App/MIS-Prog-App/mainwindow.xaml.cs
@@ -25,29 +25,22 @@ namespace MIS_Prog_App
     {
         public MainWindow()
         {
-            events = new List<Event>();
-            InitializeComponent();
 
-            string[] calendarExp = File.ReadAllLines("CalendarExport.csv");
-            for (int i = 1; i < calendarExp.Length; i++)
-            {
-                string thatLine = calendarExp[i];
-                string[] elements = thatLine.Split(",");
-                Event ev = new Event(elements[0], elements[1], elements[2], elements[3]);
-                events.Add(ev);
-            }
-            this.eventsListBox.ItemsSource = events;
         }
 
         private void AdminButton_Click(object sender, RoutedEventArgs e)
         {
             Window adminWin = new Admin();
+            adminWin.Show();
+            this.Close();
             
         }
 
         private void StudentButton_Click(object sender, RoutedEventArgs e)
         {
             Window studentWin = new Student();
+            studentWin.Show();
+            this.Close();
         }
     }
 }
