b5f1dc5281153ce2f4ede7b70e54610fcfdaf653
Pablo-Gutierrez24 <pablogutierrezfelix@gmail.com>
2022-11-02 23:50:11 -0500

    password

---
+++ MIS-Prog-App/MIS-Prog-App/Passwordwindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MessageBox = System.Windows.Forms.MessageBox;
+
+namespace MIS_Prog_App
+{
+    /// <summary>
+    /// Interaction logic for Passwordwindow.xaml
+    /// </summary>
+    public partial class Passwordwindow : Window
+    {
+        public Passwordwindow()
+        {
+
+            InitializeComponent();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            
+            if (pwdBox.Password == "mis2022")
+            {
+                Window adminWin = new Admin();
+                adminWin.Show();
+                this.Close();
+            }
+
+            else
+            {
+                MessageBox.Show("Incorrect password", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
